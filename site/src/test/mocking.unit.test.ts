e8b29c67772dac043d88aa29a5f99d85b021d4ed
Jason Olson <jolson88@outlook.com>
2023-11-05 13:55:02 -0800

    Add mock test

--- site/src/test/mocking.unit.test.ts
+++ site/src/test/mocking.unit.test.ts
@@ -1,18 +1,33 @@
-import { describe, expect, it, vi } from "vitest";
-import { mock } from "./mocking";
+import { describe, expect, it, vi } from 'vitest';
+import { mock, mockPassthrough } from './mocking';
 
 interface FooService {
   foo(): string;
   bar(): string;
 }
 
-describe("mock", () => {
-  it("should be mockable and not fail unmocked methods", () => {
+describe('mock', () => {
+  it('should be mockable and not fail unmocked methods', () => {
     const myMock = mock<FooService>({
-      foo: vi.fn().mockReturnValue("Forks!"),
+      foo: vi.fn().mockReturnValue('Forks!'),
     });
 
-    expect(myMock.foo()).toBe("Forks!");
+    expect(myMock.foo()).toBe('Forks!');
     expect(() => myMock.bar()).not.toThrow();
+  });
+
+  it('should be mockable and passthrough other methods', () => {
+    const myMock = mockPassthrough<FooService>(
+      {
+        foo: vi.fn().mockReturnValue('Forks!'),
+      },
+      {
+        foo: () => 'foo',
+        bar: () => 'bar',
+      }
+    );
+
+    expect(myMock.foo()).toBe('Forks!');
+    expect(myMock.bar()).toBe('bar');
   });
 });
