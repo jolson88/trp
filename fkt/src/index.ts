f4f6be8fc74b78e9056bf39926eef9fabeab3205
Jason Olson <jolson88@outlook.com>
2023-11-11 21:26:41 -0800

    New fact

--- fkt/src/index.ts
+++ fkt/src/index.ts
@@ -11,7 +11,8 @@ function createAllNotes(): Array<Note> {
   return [
     {
       title: "Instructional Hierarchy",
-      summary: "Acquisition, Fluency, Generalization, and Adaptation. The instructional hierarchy is a model of skill acquisition premised on the theory that children will best acquire skills through progressive instructional techniques beginning with content accuracy. Upon demonstrating adequate competency of the content (i.e., accuracy), instruction should then focus on training the learner to become fluent in the skill (i.e., high rates of accurate responding during timed probes). As fluency is achieved, the instructional hierarchy progresses to the promotion of the generalization of the skill to meaningful educational contexts. The final goal of the instructional hierarchy is to foster adaptation of the target skill to novel contexts and demands.",
+      summary:
+        "Acquisition, Fluency, Generalization, and Adaptation. The instructional hierarchy is a model of skill acquisition premised on the theory that children will best acquire skills through progressive instructional techniques beginning with content accuracy. Upon demonstrating adequate competency of the content (i.e., accuracy), instruction should then focus on training the learner to become fluent in the skill (i.e., high rates of accurate responding during timed probes). As fluency is achieved, the instructional hierarchy progresses to the promotion of the generalization of the skill to meaningful educational contexts. The final goal of the instructional hierarchy is to foster adaptation of the target skill to novel contexts and demands.",
       id: "https://jolson88.com/graph/learning/instructional-hierarchy",
       references: [
         "https://link.springer.com/referenceworkentry/10.1007/978-0-387-79061-9_3164",
